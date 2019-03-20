1ef2ec4725813d4cc334f812fee2aba01b6476c6
LynnBui <katherine.bui.304@gmail.com>
2019-03-20 15:00:17 -0700

    today

--- HW3/Hw3c.py
+++ HW3/Hw3c.py
@@ -22,18 +22,20 @@ def HammingDistance(pat1,pat2):
 
 # Calculate Distance Pattern Dna function
 def CDPD(data,pattern):
-    dist_score=len(pattern) # The maximum dist_score
-    bestPattern=""
     motif=[]
+    count=[]
     for i in range(len(data)):
+        bestPattern = ""
+        dist_score = len(pattern)  # The maximum dist_score
         for j in range(len(data[i])-len(pattern)+1):
             possiblePattern=data[i][j:j+len(pattern)]
             ds=HammingDistance(pattern,possiblePattern)
             if ds < dist_score:
                 dist_score = ds #update the dist_score
                 bestPattern = possiblePattern
-        motif.append((bestPattern))
-    return motif
+        motif.append(bestPattern)
+        count.append(dist_score) # Return an array of count
+    return str(motif)+'\n'+'Score: '+str(sum(count)) # To concatenate, outputs should have the same type
 
 pattern = "AAA"
 DNA_data = readFile("Dna.txt")
